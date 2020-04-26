d11313ac457f5ed5b56337f5b0dd5813613f972d
heather <heatherlynn1337@gmail.com>
2020-04-26 13:33:05 -0700

    adding data to server to send to client

--- public/parle.js
+++ public/parle.js
@@ -1,25 +1,46 @@
 const flipButton = document.querySelector('.card-flip');
 const flashCard = document.querySelector('.flash-card-content');
 const nextButton = document.querySelector('.next-card');
+const languageButton = document.querySelector('.choose-language');
 
 let cardWord = '';
-const wordList =[{'front': 'Bonjour', 
-'back': 'Hello'}, {'front': 'Au Revoir', 
-'back': 'Bye'},{'front': 'Merci',
-'back': 'Thank You'}]
+// const wordList =[{'front': 'Bonjour', 
+// 'back': 'Hello'}, {'front': 'Au Revoir', 
+// 'back': 'Bye'},{'front': 'Merci',
+// 'back': 'Thank You'}]
 let currentCard = 0;
+let wordList = null;
+
+languageButton.addEventListener("click", (e) => {
+  fetch("/french")
+    .then((res) => {
+        return res.json();
+    })
+    .then((words) => {
+        wordList = words;
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+})
 
 flipButton.addEventListener("click", (e) => {
-//   cardWord = flashCard.textContent;
-//   flashCard.innerText = 'Hello';
   flashCard.innerText = wordList[currentCard].back;
   currentCard++;
 });
 
 nextButton.addEventListener("click", (e) => {
-//   flashCard.innerText = "Au Revoir";
   flashCard.innerText = wordList[currentCard].front;
 });
+
+
+// fetch('http:localhost:3000/french')
+//   .then((res)=> {
+//       console.log(res);
+// })
+
+
+
 
 //starts with french
 //press button and flips card
@@ -27,4 +48,8 @@ nextButton.addEventListener("click", (e) => {
 //press button again and flips card to english
 
 //move to next card 
+
+//add typing option to check result 
+//keep track of score
+//add previous card button
 
--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+const wordList =[{'front': 'Bonjour', 
+'back': 'Hello'}, {'front': 'Au Revoir', 
+'back': 'Bye'},{'front': 'Merci',
+'back': 'Thank You'}]
 // app.get('/', (req, res) => res.send('Bonjour tout le monde!'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-//Static page for site
-// app.use('/public', express.static('./public'));
+app.get('/french', (req, res) => {
+    console.log(wordList)
+    res.send(wordList)
+})
+
+app.get("/french", (req, res) => {
+    res
+      .json(wordList)
+      .end();
+  });
+
+app.use('/public', express.static('./public'));
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
