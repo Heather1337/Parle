99afde71b57dfd8cc17b00d891c09e3b4a58aa45
heather <heatherlynn1337@gmail.com>
2020-04-26 10:58:19 -0700

    connecting server to project

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Bonjour tout le monde!'))
+// app.get('/', (req, res) => res.send('Bonjour tout le monde!'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+//Static page for site
+// app.use('/public', express.static('./public'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
